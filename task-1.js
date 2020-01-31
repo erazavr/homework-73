db523ae43df4b1a9c2d3eadc0ebb214fa4861130
Ernur Kurmanbekov <erazavr85@gmail.com>
2020-01-31 17:53:29 +0600

    Remove text 'Hello' and changed text 'Hello word' to 'Send GET request'

--- task-1.js
+++ task-1.js
@@ -4,10 +4,10 @@ const app = express();
 const port = 8000;
 
 app.get('/', (req, res)=> {
-    res.send('Hello world')
+    res.send('Send GET request')
 });
 app.get('/:text', (req, res) => {
-    res.send('Hello ' + req.params.text)
+    res.send(req.params.text)
 });
 app.listen(port, () => {
     console.log('We are live on ' + port)
